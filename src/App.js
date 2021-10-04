d469e74be89f75fdf44f3d8dc52583c3472a5a14
jpoulin587 <jpoulin587a@gmail.com>
2021-10-04 09:26:16 -0500

    clearning checked items

--- src/App.js
+++ src/App.js
@@ -39,14 +39,18 @@ function App() {
     todoNameRef.current.value = null
   }
   
+  function handleClearTodos () {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   
   return (
     <>
     <TodoList todos={todos} toggleTodo={toggleTodos} />
     <input ref={todoNameRef} type="text" />
     <button onClick={handleAddTodo}>Add todo</button>
-    <button>Clear completed</button>
-    <div> 0 todos remaining</div>
+    <button onClick={handleClearTodos}>Clear completed</button>
+    <div> {todos.filter(todo => !todo.complete).length} todos remaining</div>
 
       </>
   )
